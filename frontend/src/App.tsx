bf91e9dfebd2e83c4273916d86e659be482d3917
Lukas Schüler <unknown>
2021-06-17 12:05:55 +0200

    used App.tsx as root component and fixed a UI inconsistancy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import './App.css';
-import logo from './logo.svg';
+import Post from './component/Post';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Center'>
+      <Post
+        name={'Alexander Peterson'}
+        time={'4:44'}
+        content={'Pommes und Disco'}
+        likes={420}
+        dislikes={69}
+        isComment={false}
+      />
+      <Post
+        name={'Alexander Peterson'}
+        time={'4:44'}
+        content={'Pommes und Disco'}
+        likes={420}
+        dislikes={69}
+        isComment={true}
+      />
     </div>
   );
 }
