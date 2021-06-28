6564f967feceb170279ee55972b00e0e252f499d
Lukas Schüler <>
2021-06-28 19:09:42 +0200

    Added a Banner component

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
+import { Banner } from './component/Banner';
 import { Button } from './component/Button';
 import { TextInput, TextInputType } from './component/TextInput';
 import './Login.css';
 
 export function Login() {
   return (
-    <div className='Center'>
-      <div className='Heading'>Login</div>
-      <div>
-        <TextInput placeholder={'Name'} type={TextInputType.TEXT}></TextInput>
-      </div>
-      <div>
-        <TextInput placeholder={'Passwort'} type={TextInputType.PASSWORD}></TextInput>
-      </div>
-      <div>
-        <Button label={'Anmelden'}></Button>
+    <div>
+      <Banner></Banner>
+      <div className='Center'>
+        <div className='Heading'>Login</div>
+        <div>
+          <TextInput placeholder={'Name'} type={TextInputType.TEXT}></TextInput>
+        </div>
+        <div>
+          <TextInput placeholder={'Passwort'} type={TextInputType.PASSWORD}></TextInput>
+        </div>
+        <div>
+          <Button label={'Anmelden'}></Button>
+        </div>
       </div>
     </div>
   );
---
+++ frontend/src/component/Banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Banner.css';
+
+export function Banner() {
+  return (
+    <div className='Banner'>
+      <div className='BannerName'>Web 2.0</div>
+    </div>
+  );
+}
