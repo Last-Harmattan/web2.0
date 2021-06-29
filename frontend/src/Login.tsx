081eb868d93f3fa6314b0c3900e5a69e0c5d0b9b
Lukas Schüler <unknown>
2021-06-29 16:49:58 +0200

    Signup erstellt und Links hinzugefügt

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -8,7 +8,7 @@ export function Login() {
   return (
     <div>
       <Banner></Banner>
-      <div className='Center'>
+      <div className='Content'>
         <div className='Heading'>Login</div>
         <div>
           <TextInput placeholder={'Name'} type={TextInputType.TEXT}></TextInput>
@@ -16,9 +16,13 @@ export function Login() {
         <div>
           <TextInput placeholder={'Passwort'} type={TextInputType.PASSWORD}></TextInput>
         </div>
-        <div>
+        <div className='Button-Margin'>
           <Button label={'Anmelden'}></Button>
         </div>
+        <a href='signuppage.com' className='Registration-Link'>
+          {/*durch den Link ersetzen, der zur Registrierung führt*/}
+          Registrieren
+        </a>
       </div>
     </div>
   );
