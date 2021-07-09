158102e4403b0a812e6908fdf6cbc8c52ae9b858
Marvin Davieds <marvin.davieds@gmail.com>
2021-07-09 19:00:45 +0200

    initialize user state in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,24 @@
 import { createBrowserHistory } from 'history';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
 import { Banner } from './component/Banner';
 import { Feed } from './pages/Feed';
 import { Signup } from './pages/Signup';
 import { RootState } from './state/reducers';
+import { AppDispatch } from './state/store';
+import { initUser } from './state/userSlice';
 
 const customHistory = createBrowserHistory();
 export function App() {
+  const dispatch: AppDispatch = useDispatch();
+
   const isLoggedIn = useSelector((state: RootState) => !!state.user.currentUser);
+
+  useEffect(() => {
+    // We need the user state everywhere in our app so initialize it here.
+    dispatch(initUser());
+  }, [dispatch]);
 
   return (
     <div>
