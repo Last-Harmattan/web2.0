a6f84ee78f1de917a3999d6720610ae29a08dd73
Jan Hasselberg <jan.hasselberg@uni-rostock.de>
2021-07-09 15:25:10 +0200

    some api calls. documentation follows.

--- backend/main.py
+++ backend/main.py
@@ -20,13 +20,17 @@ cur = con.cursor()
 def init():
 
     def createDB():
-        sql_query = """
+        cur.execute("""
         CREATE TABLE IF NOT EXISTS users (
         id VARCHAR(36),
-        nick VARCHAR(30),
-        sessionid VARCHAR(30),
-        PRIMARY KEY (id));"""
-        cur.execute(sql_query)
+        username VARCHAR(30),
+        location VARCHAR(30),
+        PRIMARY KEY (id));""")
+        con.commit()
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS friendReq (
+        friendA VARCHAR(36),
+        friendB VARCHAR(36));""")
         con.commit()
 
     createDB()
@@ -45,7 +49,7 @@ def api_getAllUsers():
         content=cur.fetchall()
         erg = {}
         for e in content:
-            erg[e[0]] = {"nick":e[1],"location":e[-1]}
+            erg[e[0]] = {"username":e[1],"location":e[-1]}
         return json.dumps(erg)
 
     response.headers['Content-Type'] = 'application/json'
@@ -57,40 +61,35 @@ def api_getAllUsers():
 @app.route('/api/call/createuser')
 @app.route('/api/call/createUser')
 def api_insertUser():
-
-    def insertUserToDB(id,nick,sessionid):
-        cur.execute("INSERT INTO users (id,nick,sessionid) VALUES (?,?,?)", (id, nick, sessionid))
+    def insertUserToDB(id,username,location):
+        cur.execute("INSERT INTO users (id,username,location) VALUES (?,?,?)", (id, username, location))
         con.commit()
-
     try:
-        name = request.query['name']
-        location = request.query['location']
+        name = request.query['USERNAME']
+        location = request.query['LOCATION']
     except:
-        return HTTPResponse(status=502, body="502: query incomplete")
+        return HTTPResponse(status=502, body="502: query incomplete, username and location is necessary")
     response.headers['Content-Type'] = 'application/json'
-
     id = str(uuid.uuid4())
     insertUserToDB(id,name,location)
-
     return json.dumps({"id":id})
 
 # route for search functionality
 @app.route('/api/call/search')
 def api_search():
-
     def queryToJSON(query):
         re = []
         for e in query:
-            re.append({"id":e[0],"nick":e[1],"location":e[-1]})
+            re.append({"id":e[0],"username":e[1],"location":e[-1]})
         return(json.dumps(re))
 
     def getUsersByQuery(query):
-        cur.execute("select * from users where nick = ?", [query])
+        cur.execute("select * from users where username = ?", [query])
         content=cur.fetchall()
         return(queryToJSON(content))
 
     try:
-        name = request.query['name']
+        name = request.query['USERNAME']
     except:
         return HTTPResponse(status=502, body="query incomplete")
     response.headers['Content-Type'] = 'application/json'
@@ -106,6 +105,33 @@ def upPublicKey():
         return HTTPResponse(status=502, body="wrong file extension")
     file_path = "keys/{file}".format(file=ID+".pem")
     key.save(file_path)
+
+# route for update user location
+@app.route('/api/call/uplocation', method="GET")
+def upLocation():
+    location = request.query['LOCATION']
+    id = request.query['ID']
+    cur.execute("update users set location = ? where id = ?", (location, id))
+
+# route to get a peerjsid by given userid
+@app.route('/api/call/getLocation', method="GET")
+@app.route('/api/call/getlocation', method="GET")
+def getLocation():
+    id = request.query['ID']
+    cur.execute("select location from users where id = ?", [id])
+    content=cur.fetchall()
+    response.headers['Content-Type'] = 'application/json'
+    return(json.dumps(content))
+
+@app.route('/api/call/friendreq', method="GET")
+@app.route('/api/call/friendReq', method="GET")
+def friendReq():
+    friendA = request.query['FROM']
+    friendB = request.query['TO']
+    cur.execute("INSERT INTO friendReq (friendA,friendb) VALUES (?,?)", (friendA, friendB))
+    con.commit()
+
+
 
 init()
 
