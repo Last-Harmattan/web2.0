5ee2ff6292cf1dd6da56a4f9068ddc8796fb2c6a
Julian <julianohland@web.de>
2021-07-10 17:22:25 +0200

    removed db_tests from start screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
 import { Banner } from './component/Banner';
-import { UserDbTestComponent } from './database/UserDbTestComponent';
 import { Feed } from './pages/Feed';
 import { Signup } from './pages/Signup';
 import { initFriends } from './state/friendsSlice';
@@ -29,8 +28,6 @@ export function App() {
     <div>
       <Router history={customHistory}>
         <Banner></Banner>
-
-        <UserDbTestComponent></UserDbTestComponent>
 
         <Switch>
           <Route path='/signup'>{isLoggedIn ? <Redirect to='/' /> : <Signup />}</Route>
