41ed6e55dab89d0fa58a468590e7b59dab8151ea
Marvin Davieds <marvin.davieds@gmail.com>
2021-07-12 17:52:00 +0200

    frontend: show username in header

--- frontend/src/component/Banner.tsx
+++ frontend/src/component/Banner.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './Banner.module.css';
 
-export function Banner() {
+export interface BannerProps {
+  userName?: string;
+}
+
+export function Banner(props: BannerProps) {
   return (
     <div className={styles.Banner}>
-      <div className={styles.BannerName}>Web 2.0</div>
+      <div className={styles.BannerName}>
+        <div className={styles.Title}>Web 2.0</div>
+        <div className={styles.UserName}>{props.userName}</div>
+      </div>
     </div>
   );
 }
